02b14504835adddc2e18244328baa81daf989924
jmlaixuexi <1072006142@qq.com>
2022-10-17 10:44:52 +0800

    nacos客户端实现

---
+++ cloud-spring-alibaba-demo/user-server-nacos/src/main/java/com/jml/user/UserNacosApplication.java
@@ -0,0 +1,13 @@
+package com.jml.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserNacosApplication.class,args);
+    }
+
+}
---
+++ cloud-spring-demo/order-server-ribbon-loadbalancer/src/main/java/com/jml/order/model/User.java
@@ -0,0 +1,17 @@
+package com.jml.order.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long id;
+
+    private String username;
+
+    private String address;
+
+}
